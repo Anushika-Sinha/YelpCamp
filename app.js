dd2669159631f2860b2d49b9727cca8654c71a4d
root <sinhaanushika@gmail.com>
2020-06-03 20:16:20 +0000

    update dburl

--- app.js
+++ app.js
@@ -15,15 +15,17 @@ var commentRoutes = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes = require("./routes/index")
 
+//mongodb://localhost:27017/yelp_camp_12dep
 //seedDB();
-//mongoose.connect("mongodb://localhost:27017/yelp_camp_12dep", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-mongoose.connect("mongodb+srv://dbUser:Pp.eAmztci7p!65@cluster0-pqu7q.mongodb.net/test?retryWrites=true&w=majority",
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+/*mongoose.connect("mongodb+srv://dbUser:Pp.eAmztci7p!65@cluster0-pqu7q.mongodb.net/test?retryWrites=true&w=majority",
 {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}
 				).then(() => {
 	console.log("Connected to DB");
 }).catch(err => {
 	console.log("ERROR:", err.message);
 });
+*/
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
